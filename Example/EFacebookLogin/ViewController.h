f5aa4fcbcc38b5d273a7596be9056e3be80f3c61
brunobasas <brunobasas@gmail.com>
2014-10-09 12:04:20 +0200

    first commit

---
+++ Example/EFacebookLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EFacebookLogin
+//
+//  Created by Bruno on 09/10/14.
+//  Copyright (c) 2014 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Example/EFacebookLogin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EFacebookLogin
+//
+//  Created by Bruno on 09/10/14.
+//  Copyright (c) 2014 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
